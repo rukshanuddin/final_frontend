aafd7f7308336b5ec92e885202c647e00ff316c0
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-07-28 00:59:50 -0400

    topicAvtions (fetch)

---
+++ src/actions/topicActions.js
@@ -0,0 +1,9 @@
+export const fetchTopics = () => {
+  return (dispatch) => {
+    return fetch("http://localhost:3000/api/v1/topics")
+      .then((resp) => resp.json())
+      .then((topics) => {
+        dispatch({ type: "FETCH_TOPICS", payload: topics });
+      });
+  };
+};
