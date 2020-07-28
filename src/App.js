c4154d7711ce1c1c9fd8d58f21b44895c9c7f3a5
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-07-27 22:54:17 -0400

    setting up for redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {!this.props.userReducer.loggedIn ? (
+          <h1>Sign Up or Login!</h1>
+        ) : (
+          <h1>Welcome, {this.props.userReducer.user.first_name}</h1>
+        )}
+        <Signup />
+        <Login />
+        <button>Logout</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    userReducer: state.userReducer,
+  };
+};
+
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { Provider } from "react-redux";
-import rootReducer from "./reducers/index";
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers'
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
@@ -15,3 +16,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
