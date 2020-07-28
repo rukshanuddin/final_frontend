69c170f45f129e5623876d63d2191c61f9eba0ea
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-07-28 19:11:59 -0400

    simplyfy

--- src/reducers/linkReducer.js
+++ src/reducers/linkReducer.js
@@ -1,11 +1,7 @@
-export default (state = [], action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case "FETCH_LINK":
-      return [...action.payload];
-    case "FETCH_LINKS":
-      return [...action.payload];
-    case "FETCH_TOPIC_LINKS":
-      return [...action.payload];
+      return action.payload;
     default:
       return state;
   }
---
+++ src/reducers/linksReducer.js
@@ -0,0 +1,10 @@
+export default (state = [], action) => {
+  switch (action.type) {
+    case "FETCH_LINKS":
+      return [...action.payload];
+    case "FETCH_TOPIC_LINKS":
+      return [...action.payload];
+    default:
+      return state;
+  }
+};
