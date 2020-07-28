c98a801d6711e96b8178465d2ac6da10da69207b
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-07-27 22:54:37 -0400

    set up user actions I need so far

---
+++ src/actions/userActions.js
@@ -0,0 +1,35 @@
+const setUser = (payload) => ({ type: "SET_USER", payload });
+
+export const logUserOut = () => ({ type: "LOG_OUT" });
+
+export const fetchUser = (userInfo) => (dispatch) => {
+  fetch(`http://localhost:3000/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({auth: userInfo}),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      localStorage.setItem("token", data.jwt);
+      dispatch(setUser(data.user));
+    });
+};
+
+export const signUserUp = (userInfo) => (dispatch) => {
+  fetch(`http://localhost:3000/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userInfo),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      localStorage.setItem("token", data.jwt);
+      dispatch(setUser(data.user));
+    });
+};
