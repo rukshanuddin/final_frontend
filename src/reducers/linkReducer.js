3fade15a48dcc8b7a2b9f6988ec1c17b75ce76e9
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-07-28 12:40:33 -0400

    link reducer fin

--- src/reducers/linkReducer.js
+++ src/reducers/linkReducer.js
@@ -1,8 +1,12 @@
 export default (state = [], action) => {
   switch (action.type) {
+    case "FETCH_LINK":
+      return [...action.payload];
+    case "FETCH_LINKS":
+      return [...action.payload];
+    case "FETCH_TOPIC_LINKS":
+      return [...action.payload];
     default:
       return state;
   }
 };
-
-
