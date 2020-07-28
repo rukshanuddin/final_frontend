5dbc0f953474d01b1729bfaa4eb7410724296a61
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-07-28 19:11:29 -0400

    fixed some things

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,18 @@
 import userReducer from "./userReducer";
 import comments from "./commentReducer";
-import links from "./linkReducer";
-import topics from "./topicReducer";
+import links from "./linksReducer";
+import link from "./linkReducer";
+import topics from "./topicsReducer";
+import topic from "./topicReducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   userReducer,
   comments,
   links,
+  link,
   topics,
+  topic
 });
 
 export default rootReducer;
