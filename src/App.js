fcca40af6064888b5748a6200253223db22ce2bc
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-07-29 12:09:38 -0400

    changedapp/ backup of old app file

--- src/App.js
+++ src/App.js
@@ -1,52 +1,14 @@
-import React, {Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
-import { connect } from "react-redux";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import TopicList from "./containers/TopicList";
-import AllLinks from "./containers/AllLinks"
-import TopicShow from './containers/TopicShow'
+import React from "react";
+import PropTypes from "prop-types";
+import { ConnectedRouter } from "connected-react-router";
+import routes from "./routes";
 
-
-class App extends Component {
-
-  componentDidMount() {}
-
-  render() {
-    return (
-      <div className="App">
-        {!this.props.userReducer.loggedIn ? (
-          <h1>Sign Up or Login!</h1>
-        ) : (
-          <h1>Welcome, {this.props.userReducer.user.first_name}</h1>
-        )}
-        {/* <TopicList /> */}
-        <Router>
-          <Route exact path="/topics" component={TopicList} />
-          <Route exact path="/links" component={AllLinks} />
-          <Route
-            exact
-            path="/topics/:id"
-            render={(props) => (
-              <TopicShow {...props} topic={this.props.topics} />
-            )}
-          />
-        </Router>
-        <Signup />
-        <Login />
-        <button>Logout</button>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    userReducer: state.userReducer,
-    topicReducer: state.topicReducer,
-  };
+const App = ({ history }) => {
+  return <ConnectedRouter history={history}>{routes}</ConnectedRouter>;
 };
 
+App.propTypes = {
+  history: PropTypes.object,
+};
 
-export default connect(mapStateToProps)(App);
+export default App;
