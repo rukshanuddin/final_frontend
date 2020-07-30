0b930839aa28c3082bf582bb2d6ca3f1d05afb7c
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-07-30 14:09:02 -0400

    image to logout screen

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { logUserOut } from "../actions/userActions";
 import { Typography, Paper, Button } from "@material-ui/core";
-
+import hello from "../resources/hello.svg";
 class Logout extends Component {
   onClick = (e) => {
     e.preventDefault();
@@ -24,12 +24,37 @@ class Logout extends Component {
             marginRight: "auto",
           }}
         >
-          <Typography variant="h4" component="h3" align="center">
-            Logout
-          </Typography>
-          <Typography variant="subtitle1" component="p" align="center">
-            Are you sure you want to go?
-          </Typography>
+          <Paper
+            square={true}
+            elevation={12}
+            style={{
+              padding: "1em",
+              marginTop: "1em",
+              width: "50%",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <Typography variant="h3" component="h3" align="center">
+              Logout
+            </Typography>
+            <Typography variant="subtitle1" component="p" align="center">
+              <em>Are you sure you want to go?</em>
+            </Typography>
+          </Paper>
+          <Paper
+            square={true}
+            elevation={12}
+            style={{
+              padding: "1em",
+              marginTop: "1em",
+              width: "70%",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <img src={hello} alt="welcome" style={{ height: "25em" }} />
+          </Paper>
           <Paper style={{ marginTop: "1em", padding: "1em" }}>
             <Button
               variant="contained"
